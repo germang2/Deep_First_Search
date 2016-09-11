794a214050689ac3cf2aac3f9a8bebffa8695039
germang2 <germandg99@gmail.com>
2016-09-11 13:40:40 -0500

    Prueba2

---
+++ form.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class tesoro(forms.Form):
+	Tesoro = forms.CharField(max_length=1)
--- views.py
+++ views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, render_to_response
-#from form import tesoro
+from form import tesoro
 #from dfs import main
 
 def home(request):
-	context = {}
+	form = tesoro(request.POST or None)
+	text = ""
+	if form.is_valid():
+		data = form.cleaned_data
+		t = data.get('Tesoro')
+		#print t
+		#text = main(t)
+		#print text
+	context = {
+		'form':form,
+		'text':text
+	}
 	return render(request,"inicio.html",context)
